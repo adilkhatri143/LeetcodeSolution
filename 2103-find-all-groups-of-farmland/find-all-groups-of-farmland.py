db4fa6aef82a4e1331a3bb5e87d907bf984c25de
adilkhatri143 <adilkhatri143@gmail.com>
2024-04-20 20:16:34 +0530

    Time: 909 ms (43.00%) | Memory: 32.8 MB (47.00%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
@@ -2,7 +2,6 @@ class Solution:
     def solve(self, row, col, rows, cols, land):
         if row < 0 or row >= rows or col < 0 or col >= cols or land[row][col] == -1 or land[row][col] == 0:
             return float('-inf'), float('-inf')
-
         land[row][col] = -1
         left_row, left_col = self.solve(row, col + 1, rows, cols, land)
         bottom_row, bottom_col = self.solve(row + 1, col, rows, cols, land)
@@ -16,8 +15,7 @@ class Solution:
             for col in range(cols):
                 if land[row][col] == 1:
                     temp = [row, col]
-                    end_boundary = self.solve(row, col, rows, cols, land)
-                    temp.extend(end_boundary)
+                    temp.extend(self.solve(row, col, rows, cols, land))
                     ans.append(temp)
         return ans
                 
