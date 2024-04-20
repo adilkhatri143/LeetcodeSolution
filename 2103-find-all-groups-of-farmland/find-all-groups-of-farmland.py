bb9c5f09ff7ca80682d58b08deb6cbfb2bda27b8
adilkhatri143 <adilkhatri143@gmail.com>
2024-04-20 20:45:12 +0530

    Time: 750 ms (94.00%) | Memory: 29.8 MB (91.00%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
@@ -1,6 +1,6 @@
 class Solution:
     def solve(self, row, col, rows, cols, land):
-        if row < 0 or row >= rows or col < 0 or col >= cols or land[row][col] == -1 or land[row][col] == 0:
+        if row >= rows or col >= cols or land[row][col] == -1 or land[row][col] == 0:
             return float('-inf'), float('-inf')
         land[row][col] = -1
         left_row, left_col = self.solve(row, col + 1, rows, cols, land)
@@ -13,9 +13,13 @@ class Solution:
         ans = list()
         for row in range(rows):
             for col in range(cols):
-                if land[row][col] == 1:
-                    temp = [row, col]
-                    temp.extend(self.solve(row, col, rows, cols, land))
-                    ans.append(temp)
+                if land[row][col] == 1 and (row == 0 or land[row - 1][col] == 0) and (col == 0 or land[row][col - 1] == 0):
+                    bottom_row = row
+                    right_col = col
+                    while bottom_row + 1 < rows and land[bottom_row + 1][col] == 1:
+                        bottom_row += 1
+                    while right_col + 1 < cols and land[row][right_col + 1] == 1:
+                        right_col += 1
+                    ans.append([row, col, bottom_row, right_col])
         return ans
                 
