c9f3a5f53fc4737b080debcbf8ebd8532d6f4490
adilkhatri143 <adilkhatri143@gmail.com>
2024-04-25 20:01:55 +0530

    Time: 2053 ms (12.21%) | Memory: 81 MB (13.26%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,27 +1,25 @@
 class Solution:
     def solve(self, idx, buy, prices, k, dp):
-        if idx == len(prices):
-            return 0
-        if k == 0:
+        if idx == len(prices) or k == 0:
             return 0
 
-        if (idx, buy, k) in dp:
-            return dp[(idx, buy, k)]
+        if dp[idx][buy][k] != -1:
+            return dp[idx][buy][k]
 
         if buy == 1:
-            dp[(idx, buy, k)] = max(
+            dp[idx][buy][k] = max(
                 -prices[idx] + self.solve(idx + 1, buy - 1, prices, k, dp),
                 self.solve(idx + 1, buy, prices, k, dp)
             )
-            return dp[(idx, buy, k)]
+            return dp[idx][buy][k]
         else:
-            dp[(idx, buy, k)] = max(
+            dp[idx][buy][k] = max(
                 prices[idx] + self.solve(idx + 1, buy + 1, prices, k - 1, dp),
                 self.solve(idx + 1, buy, prices, k, dp)
             )
-            return dp[(idx, buy, k)]
+            return dp[idx][buy][k]
         
 
     def maxProfit(self, prices: List[int]) -> int:
-        dp = dict()
+        dp = [[[-1 for i in range(3)] for j in range(2)] for k in range(len(prices))]
         return self.solve(0, 1, prices, 2, dp)
