036c618e42f099e7efbeb22256fb85faf5e3dd4c
adilkhatri143 <adilkhatri143@gmail.com>
2024-04-25 20:05:02 +0530

    Time: 187 ms (29.07%) | Memory: 21.5 MB (40.03%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,25 @@
+class Solution:
+    def solve(self, idx, buy, prices, k, dp):
+        if idx == len(prices) or k == 0:
+            return 0
+
+        if dp[idx][buy][k] != -1:
+            return dp[idx][buy][k]
+
+        if buy == 1:
+            dp[idx][buy][k] = max(
+                -prices[idx] + self.solve(idx + 1, buy - 1, prices, k, dp),
+                self.solve(idx + 1, buy, prices, k, dp)
+            )
+            return dp[idx][buy][k]
+        else:
+            dp[idx][buy][k] = max(
+                prices[idx] + self.solve(idx + 1, buy + 1, prices, k - 1, dp),
+                self.solve(idx + 1, buy, prices, k, dp)
+            )
+            return dp[idx][buy][k]
+        
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        dp = [[[-1 for i in range(k + 1)] for j in range(2)] for _ in range(len(prices))]
+        return self.solve(0, 1, prices, k, dp)
+    
