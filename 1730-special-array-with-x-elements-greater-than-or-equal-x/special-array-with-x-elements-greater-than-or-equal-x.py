c0a90c38d6f1e50bcb09db193008f95533a4ce60
adilkhatri143 <adilkhatri143@gmail.com>
2024-05-28 17:30:35 +0530

    Time: 41 ms (56.47%) | Memory: 16.4 MB (98.87%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,11 +1,33 @@
 class Solution:
+    def get_count(self, target, nums):
+        count = 0
+        for num in nums:
+            if num >= target:
+                count += 1
+        if count == target:
+            return 0, target
+        return -1, count
+
     def specialArray(self, nums: List[int]) -> int:
+        # binary_search(0, max_num, nums)
+        # for x in range(max_num + 1):
+        #     count = 0
+        #     for num in nums:
+        #         if num >= x:
+        #             count += 1
+        #     if count == x:
+        #         return 0, x
+        # return -1, count
         max_num = max(nums)
-        for x in range(max_num + 1):
-            count = 0
-            for num in nums:
-                if num >= x:
-                    count += 1
-            if count == x:
-                return x
+        start = 0
+        end = max_num
+        while start <= end:
+            mid = (start + end) // 2
+            conti, answer = self.get_count(mid, nums)
+            if conti != -1:
+                return answer
+            elif answer >= mid:
+                start = mid + 1
+            else:
+                end = mid - 1
         return -1
