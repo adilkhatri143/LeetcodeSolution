ed49d9f17b9e13b737d4a8eedbb94be53cb2f9b4
adilkhatri143 <adilkhatri143@gmail.com>
2024-05-30 13:05:55 +0530

    Time: 199 ms (86.70%) | Memory: 19.9 MB (35.25%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.py
+++ 3451-string-compression-iii/string-compression-iii.py
@@ -1,20 +1,24 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-        st = list()
         count = 0
-        for ch in word:
-            if st and st[-1][0] == ch:
-                top_data = st.pop()
-                st.append((top_data[0], top_data[1] + 1))
-            else:
-                st.append((ch, 1))
+        prev = None
         answer = ""
-        while st:
-            ch, count = st.pop()
-            time = count // 9
-            if count % 9:
-                answer = f"{count % 9}{ch}" + answer
-            for i in range(time):
-                answer = f"9{ch}" + answer
+        for ch in word:
+            if prev is None:
+                prev = ch
+                count += 1
+            elif prev and ch == prev:
+                count += 1
+            else:
+                for i in range(count // 9):
+                    answer += f"9{prev}"
+                if count % 9:
+                    answer += f"{count % 9}{prev}"
+                prev = ch
+                count = 1
+        for i in range(count // 9):
+            answer += f"9{prev}"
+        if count % 9:
+            answer += f"{count % 9}{prev}"
         return answer
 
