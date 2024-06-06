fd7acc9f5a8c54910a63509c013c82df4f87443f
adilkhatri143 <adilkhatri143@gmail.com>
2024-06-06 23:09:20 +0530

    Time: 384 ms (19.87%) | Memory: 33.9 MB (50.66%) - LeetSync

---
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.py
@@ -0,0 +1,17 @@
+import heapq
+class Solution:
+    def isPossibleDivide(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        num_to_count = Counter(hand)
+        heap = list(num_to_count.keys())
+        heapq.heapify(heap)
+        while heap:
+            min_num = heap[0]
+            for i in range(groupSize):
+                if min_num + i not in num_to_count:
+                    return False
+                num_to_count[min_num + i] -= 1
+                if num_to_count[min_num + i] == 0 and min_num + i != heapq.heappop(heap):
+                    return False
+        return True
