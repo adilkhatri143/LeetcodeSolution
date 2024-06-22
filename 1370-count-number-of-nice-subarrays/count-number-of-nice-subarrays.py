8f9ebbfa6c64d21490b5e2b8d28c597cd85a3c15
adilkhatri143 <adilkhatri143@gmail.com>
2024-06-22 19:07:40 +0530

    Time: 647 ms (29.78%) | Memory: 23.6 MB (67.27%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -1,14 +1,14 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        def extactlyK(t):
-            window_start = window_end = answer = 0
+        def solve(nums, k):
+            window_start = window_end = odd_count = answer = 0
             while window_start < len(nums):
-                t -= nums[window_start] % 2
-                while window_end <= window_start and t < 0:
-                    t += nums[window_end] % 2
+                odd_count += nums[window_start] % 2
+                while odd_count > k:
+                    odd_count -= nums[window_end] % 2
                     window_end += 1
                 answer += (window_start - window_end + 1)
                 window_start += 1
             return answer
-        
-        return extactlyK(k) - extactlyK(k - 1)
+
+        return solve(nums, k) - solve(nums, k - 1)
