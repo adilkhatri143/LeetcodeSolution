bdc58dec1b260ffab60ba787265adba17d0e610d
adilkhatri143 <adilkhatri143@gmail.com>
2024-07-05 15:04:45 +0530

    Time: 366 ms (34.22%) | Memory: 44.2 MB (82.89%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -7,7 +7,7 @@ class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
         if head.next.next is None:
             return [-1, -1]
-        min_idx = max_idx = prev_max_idx = 2
+        min_idx = max_idx = 2
         cur_idx = 2
         minf = maxf = True
         min_dis = float('inf')
@@ -20,11 +20,9 @@ class Solution:
                     minf = False
                 if max_idx == 2 and maxf:
                     max_idx = cur_idx
-                    prev_max_idx = max_idx
                     maxf = False
                 else:
                     min_dis = min(min_dis, cur_idx - max_idx)
-                    prev_max_idx = max_idx
                     max_idx = cur_idx
             elif prev_node.val > itr_node.val < itr_node.next.val:
                 if min_idx == 2 and minf:
@@ -32,16 +30,13 @@ class Solution:
                     minf = False
                 if max_idx == 2 and maxf:
                     max_idx = cur_idx
-                    prev_max_idx = max_idx
                     maxf = False
                 else:
                     min_dis = min(min_dis, cur_idx - max_idx)
-                    prev_max_idx = max_idx
                     max_idx = cur_idx
             cur_idx += 1
             prev_node = itr_node
             itr_node = itr_node.next
-        print(min_dis, max_idx, min_idx)
         return [
             min_dis if min_dis != float('inf') else -1,
             max_idx - min_idx if max_idx - min_idx != 0 else -1
