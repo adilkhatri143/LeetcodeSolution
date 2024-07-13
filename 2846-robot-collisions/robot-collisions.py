6ccced26d8ff4e08fd2ed34db83eae2d930cd03a
adilkhatri143 <adilkhatri143@gmail.com>
2024-07-13 16:40:38 +0530

    Time: 1085 ms (22.12%) | Memory: 49 MB (17.31%) - LeetSync

--- 2846-robot-collisions/robot-collisions.py
+++ 2846-robot-collisions/robot-collisions.py
@@ -7,31 +7,28 @@ class Solution:
         for p, h, d, op in positions:
             if not stack:
                 stack.append([h, d, op])
-                continue
             elif stack and stack[-1][1] == "R" and d == "R":
                 stack.append([h, d, op])
-                continue
             elif stack and stack[-1][1] == "L" and d == "L":
                 stack.append([h, d, op])
-                continue
             elif stack and stack[-1][1] == "L" and d == "R":
                 stack.append([h, d, op])
-                continue 
-            to_add = False
-            while stack and stack[-1][1] == "R" and d == "L":
-                if stack[-1][0] == h:
-                    stack.pop()
-                    to_add = False
-                    break
-                elif stack[-1][0] > h:
-                    stack[-1][0] -= 1
-                    to_add = False
-                    break
-                elif stack[-1][0] < h:
-                    stack.pop()
-                    h = h - 1
-                    to_add = True
-            if to_add:
-                stack.append([h, d, op])
+            else:
+                to_add = False
+                while stack and stack[-1][1] == "R" and d == "L":
+                    if stack[-1][0] == h:
+                        stack.pop()
+                        to_add = False
+                        break
+                    elif stack[-1][0] > h:
+                        stack[-1][0] -= 1
+                        to_add = False
+                        break
+                    elif stack[-1][0] < h:
+                        stack.pop()
+                        h = h - 1
+                        to_add = True
+                if to_add:
+                    stack.append([h, d, op])
         stack.sort(key=lambda k: k[2])
         return [h for h, d, op in stack]
