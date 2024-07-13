083e29d2130ae3988d48d2858c589f936cee886d
adilkhatri143 <adilkhatri143@gmail.com>
2024-07-13 16:39:29 +0530

    Time: 1069 ms (24.04%) | Memory: 48.3 MB (25.00%) - LeetSync

---
+++ 2846-robot-collisions/robot-collisions.py
@@ -0,0 +1,37 @@
+class Solution:
+    def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
+        for i in range(len(positions)):
+            positions[i] = (positions[i], healths[i], directions[i], i)
+        positions.sort(key=lambda x: x[0])
+        stack = list()
+        for p, h, d, op in positions:
+            if not stack:
+                stack.append([h, d, op])
+                continue
+            elif stack and stack[-1][1] == "R" and d == "R":
+                stack.append([h, d, op])
+                continue
+            elif stack and stack[-1][1] == "L" and d == "L":
+                stack.append([h, d, op])
+                continue
+            elif stack and stack[-1][1] == "L" and d == "R":
+                stack.append([h, d, op])
+                continue 
+            to_add = False
+            while stack and stack[-1][1] == "R" and d == "L":
+                if stack[-1][0] == h:
+                    stack.pop()
+                    to_add = False
+                    break
+                elif stack[-1][0] > h:
+                    stack[-1][0] -= 1
+                    to_add = False
+                    break
+                elif stack[-1][0] < h:
+                    stack.pop()
+                    h = h - 1
+                    to_add = True
+            if to_add:
+                stack.append([h, d, op])
+        stack.sort(key=lambda k: k[2])
+        return [h for h, d, op in stack]
