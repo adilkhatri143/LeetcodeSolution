4c26373ba8023f70d76f57a0480940483b90d668
adilkhatri143 <adilkhatri143@gmail.com>
2024-07-30 14:05:31 +0530

    Time: 1350 ms (12.65%) | Memory: 24.3 MB (59.76%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,42 +1,45 @@
 class Solution:
-    def merge(self, nums, left, mid, right):
-        left_arr = [0] * (mid - left + 1)
-        right_arr = [0] * (right - mid)
-
-        for i in range(mid - left + 1):
-            left_arr[i] = nums[left + i]
-        for i in range(right - mid):
-            right_arr[i] = nums[mid + i + 1]
+    def mergeSort(self, left, mid, right, nums):
+        elementInLeft = mid - left + 1
+        elementInRight = right - mid
         
-        left_ptr = right_ptr = 0
-        merge_ptr = left
-        while left_ptr < len(left_arr) and right_ptr < len(right_arr):
-            if left_arr[left_ptr] < right_arr[right_ptr]:
-                nums[merge_ptr] = left_arr[left_ptr]
-                left_ptr += 1
+        leftElements = [0] * elementInLeft
+        rightElements = [0] * elementInRight
+    
+        for i in range(elementInLeft):
+            leftElements[i] = nums[left + i]
+        for i in range(elementInRight):
+            rightElements[i] = nums[mid + 1 + i]
+        
+        leftPointer = rightPointer = 0
+        while leftPointer < elementInLeft and rightPointer < elementInRight:
+            if leftElements[leftPointer] < rightElements[rightPointer]:
+                nums[left] = leftElements[leftPointer]
+                leftPointer += 1
             else:
-                nums[merge_ptr] = right_arr[right_ptr]
-                right_ptr += 1
-            merge_ptr += 1
+                nums[left] = rightElements[rightPointer]
+                rightPointer += 1
+            left += 1
         
-        while left_ptr < len(left_arr):
-            nums[merge_ptr] = left_arr[left_ptr]
-            left_ptr += 1
-            merge_ptr += 1
+        while leftPointer < elementInLeft:
+            nums[left] = leftElements[leftPointer]
+            leftPointer += 1
+            left += 1
         
-        while right < len(right_arr):
-            nums[merge_ptr] = right_arr[right_ptr]
-            right_ptr += 1
-            merge_ptr += 1        
+        while rightPointer < elementInRight:
+            nums[left] = rightElements[rightPointer]
+            rightPointer += 1
+            left += 1
 
-    def merge_sort(self, nums, left, right):
+    def merge(self, left, right, nums):
         if left < right:
             mid = (left + right) // 2
-            self.merge_sort(nums, left, mid)
-            self.merge_sort(nums, mid + 1, right)
-            return self.merge(nums, left, mid, right)
+            self.merge(left, mid, nums)
+            self.merge(mid + 1, right, nums)
+            self.mergeSort(left, mid, right, nums)
+
 
     def sortArray(self, nums: List[int]) -> List[int]:
         left, right = 0, len(nums) - 1
-        self.merge_sort(nums, left, right)
+        self.merge(left, right, nums)
         return nums
