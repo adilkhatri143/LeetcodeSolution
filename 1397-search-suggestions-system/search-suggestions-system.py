ce7577d4b0ae84e21595757463a24ed32deca2b5
adilkhatri143 <adilkhatri143@gmail.com>
2024-08-18 01:01:28 +0530

    Time: 63 ms (94.50%) | Memory: 19.6 MB (74.04%) - LeetSync

---
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -0,0 +1,18 @@
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        answer = list()
+        products.sort()
+        left, right = 0, len(products) - 1
+        for i in range(len(searchWord)):
+            ch = searchWord[i]
+            while left <= right and (len(products[left]) <= i or products[left][i] != ch):
+                left += 1
+            while left <= right and (len(products[right]) <= i or products[right][i] != ch):
+                right -= 1
+            answer.append([])
+            window_len = right - left + 1
+            for i in range(min(window_len, 3)):
+                answer[-1].append(products[left + i])
+        return answer
+
+
