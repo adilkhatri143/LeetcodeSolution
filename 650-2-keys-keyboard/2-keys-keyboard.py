cfd1b923616ab4e5f728c14e8da9eea6de02ca38
adilkhatri143 <adilkhatri143@gmail.com>
2024-08-20 22:58:55 +0530

    Time: 152 ms (44.33%) | Memory: 17.7 MB (28.00%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.py
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -1,15 +1,19 @@
 class Solution:
-    def solve(self, chars, steps, prev, n):
+    def solve(self, chars, prev, steps, n, dp):
         if chars == n:
             return steps
         if chars > n:
             return float("inf")
-        cp_ans = self.solve(chars + chars, steps + 2, chars, n)
+        if (chars, prev) in dp:
+            return dp[(chars, prev)]
+        cp_ans = self.solve(chars + chars, chars, steps + 2, n, dp)
         p_ans = float("inf")
         if chars != 1:
-            p_ans = self.solve(chars + prev, steps + 1, prev, n)
-        return min(cp_ans, p_ans)
+            p_ans = self.solve(chars + prev, prev, steps + 1, n, dp)
+        dp[(chars, prev)] = min(cp_ans, p_ans)
+        return dp[(chars, prev)]
 
 
     def minSteps(self, n: int) -> int:
-        return self.solve(1, 0, 0, n)
+        dp = dict()
+        return self.solve(1, 0, 0, n, dp)
