9ecb1787976aa8108c4ea08b715c44597aa125f5
adilkhatri143 <adilkhatri143@gmail.com>
2024-08-20 22:56:59 +0530

    Time: 484 ms (13.38%) | Memory: 16.6 MB (61.34%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.py
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -1,19 +1,15 @@
 class Solution:
-    def solve(self, n, screen, clipboard, dp):
-        if screen == n:
-            return 0
-        if screen > n:
-            return float('inf')
-        if (screen, clipboard) in dp:
-            return dp[(screen, clipboard)]
+    def solve(self, chars, steps, prev, n):
+        if chars == n:
+            return steps
+        if chars > n:
+            return float("inf")
+        cp_ans = self.solve(chars + chars, steps + 2, chars, n)
+        p_ans = float("inf")
+        if chars != 1:
+            p_ans = self.solve(chars + prev, steps + 1, prev, n)
+        return min(cp_ans, p_ans)
 
-        copy_paste = self.solve(n, screen + screen, screen, dp) + 2
-        paste = float('inf')
-        if clipboard:
-            paste = self.solve(n, screen + clipboard, clipboard, dp) + 1
-        dp[(screen, clipboard)] = min(copy_paste, paste)
-        return dp[(screen, clipboard)]
 
     def minSteps(self, n: int) -> int:
-        dp = dict()
-        return self.solve(n, 1, 0, dp)
+        return self.solve(1, 0, 0, n)
