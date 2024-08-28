a9cfc6aa869bd424f65d78556b4105ddc09102c9
adilkhatri143 <adilkhatri143@gmail.com>
2024-08-29 00:55:30 +0530

    Time: 2069 ms (64.76%) | Memory: 41 MB (43.69%) - LeetSync

---
+++ 2035-count-sub-islands/count-sub-islands.py
@@ -0,0 +1,24 @@
+class Solution:
+    def solve(self, row, col, grid1, grid2):
+        answer = True
+        if grid1[row][col] == 0:
+            answer = False
+        grid2[row][col] = -1
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        for x, y in directions:
+            new_row = row + x
+            new_col = col + y
+            if 0 <= new_row < len(grid1) and 0 <= new_col < len(grid1[0]) and grid2[new_row][new_col] == 1:
+                answer &= self.solve(new_row, new_col, grid1, grid2)
+        return answer
+
+
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        m = len(grid1)
+        n = len(grid1[0])
+        answer = 0
+        for row in range(m):
+            for col in range(n):
+                if grid2[row][col] == 1 and self.solve(row, col, grid1, grid2):
+                    answer += 1
+        return answer
