2cc045452ea8077fca119d25f1e6ecfd3fd39c2a
adilkhatri143 <adilkhatri143@gmail.com>
2024-09-01 20:39:34 +0530

    Time: 673 ms (38.88%) | Memory: 30.4 MB (88.70%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.py
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.py
@@ -1,10 +1,11 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        answer = [0] * (n + 1)
+        answer = [0] * n
         for first, last, seat in bookings:
             answer[first - 1] += seat
-            answer[last] -= seat
+            if last < n:
+                answer[last] -= seat
         
         for i in range(1, len(answer)):
             answer[i] += answer[i - 1]
-        return answer[:-1]
+        return answer
