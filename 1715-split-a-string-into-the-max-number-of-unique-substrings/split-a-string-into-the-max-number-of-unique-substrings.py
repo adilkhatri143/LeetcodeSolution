3e9c3a517921efd7e6151c9c58d760c1a7d6a062
adilkhatri143 <adilkhatri143@gmail.com>
2024-10-21 12:47:00 +0530

    Time: 146 ms (92.38%) | Memory: 16.8 MB (6.28%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.py
@@ -5,13 +5,14 @@ class Solution:
             self.max_unique_substring = max(self.max_unique_substring, len(exist))
             return 
 
-        new_str = ""
+        new_str = []
         for i in range(idx, len(s)):
-            new_str += s[i]
-            if new_str not in exist:
-                exist.add(new_str)
+            new_str.append(s[i])
+            temp_join = "".join(new_str)
+            if temp_join not in exist:
+                exist.add(temp_join)
                 self.solve(i + 1, s, exist)
-                exist.remove(new_str)
+                exist.remove(temp_join)
                 
     def maxUniqueSplit(self, s: str) -> int:
         self.max_unique_substring = float("-inf")
