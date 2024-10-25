7ebe7afc4144629fa19022911440ba3ceda96786
adilkhatri143 <adilkhatri143@gmail.com>
2024-10-25 11:40:05 +0530

    Time: 23 ms (95.00%) | Memory: 29.6 MB (93.10%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,39 +1,10 @@
-class TreeNode():
-    def __init__(self):
-        self.childs = dict()
-        self.is_terminal = False
-    
-
-class Trie:
-    def __init__(self):
-        self.node = TreeNode()
-    
-    def insert(self, word):
-        itr = self.node
-        words = word.split("/")
-        for ch in words:
-            if ch not in itr.childs:
-                itr.childs[ch] = TreeNode()
-            itr = itr.childs[ch]
-        itr.is_terminal = True
-    
-    def search(self, word):
-        itr = self.node
-        words = word.split("/")
-        for i in range(len(words)):
-            if itr.childs[words[i]].is_terminal and i != len(words) - 1:
-                return False
-            itr = itr.childs.get(words[i])
-        return True
-
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        trie = Trie()
-        result = list()
-        for f in folder:
-            trie.insert(f)
-        for f in folder:
-            if trie.search(f):
-                result.append(f)
-        return result
-
+        folder.sort()
+        answer = []
+        prev = ""
+        for path in folder:
+            if not prev or not path.startswith(prev + "/"):
+                answer.append(path)
+                prev = path
+        return answer
