4a434a708c718786f118876234c34832eff1e002
adilkhatri143 <adilkhatri143@gmail.com>
2024-10-26 22:39:18 +0530

    Time: 399 ms (87.95%) | Memory: 64.9 MB (34.36%) - LeetSync

--- 2545-height-of-binary-tree-after-subtree-removal-queries/height-of-binary-tree-after-subtree-removal-queries.py
+++ 2545-height-of-binary-tree-after-subtree-removal-queries/height-of-binary-tree-after-subtree-removal-queries.py
@@ -5,29 +5,35 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    cur_max_height = 0
-    def treeQueries(self, root: Optional[TreeNode], queries: List[int]) -> List[int]:
-        node_to_height = dict()
-        def traverse_left_to_right(root, cur_height):
-            nonlocal node_to_height
-            if root is None:
-                return
-            node_to_height[root.val] = self.cur_max_height
-            self.cur_max_height = max(cur_height, self.cur_max_height)
-            traverse_left_to_right(root.left, cur_height + 1)
-            traverse_left_to_right(root.right, cur_height + 1)
-        
-        def traverse_right_to_left(root, cur_height):
-            nonlocal node_to_height
-            if root is None:
-                return
-            node_to_height[root.val] = max(node_to_height[root.val], self.cur_max_height)
-            self.cur_max_height = max(self.cur_max_height, cur_height)
-            traverse_right_to_left(root.right, cur_height + 1)
-            traverse_right_to_left(root.left, cur_height + 1)
-        
-        traverse_left_to_right(root, 0)
-        self.cur_max_height = 0
-        traverse_right_to_left(root, 0)
-        return [node_to_height[val] for val in queries]
+    node_to_height = dict()
+    max_heights = dict()
+    def find_max_height(self, root):
+        if root is None:
+            return -1
 
+        if root.val in self.node_to_height:
+            return self.node_to_height[root.val]
+
+        cur_node_height = 1 + max(self.find_max_height(root.left), self.find_max_height(root.right))
+        self.node_to_height[root.val] = cur_node_height
+        return self.node_to_height[root.val]
+    
+    def solve(self, root, level, max_height):
+        if root is None: return
+        self.max_heights[root.val] = max_height
+        self.solve(
+            root.left,
+            level + 1,
+            max(max_height, level + 1 + self.find_max_height(root.right))
+        )
+        self.solve(
+            root.right,
+            level + 1,
+            max(max_height, level + 1 + self.find_max_height(root.left))
+        )
+
+    def treeQueries(self, root: Optional[TreeNode], queries: List[int]) -> List[int]:
+        self.node_to_height.clear()
+        self.max_heights.clear()
+        self.solve(root, 0, 0)
+        return [self.max_heights[val] for val in queries]
