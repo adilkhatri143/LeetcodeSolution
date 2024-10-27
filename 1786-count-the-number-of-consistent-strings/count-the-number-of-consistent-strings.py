48393dadf967b4b40ad3b3979026fb5dda628aea
adilkhatri143 <adilkhatri143@gmail.com>
2024-10-27 22:06:23 +0530

    Time: 230 ms (21.15%) | Memory: 18.6 MB (10.00%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
@@ -4,7 +4,7 @@ class Solution:
         res = 0
         for word in words:
             contains_char = set(word)
-            diff = contains_char - allowed_char
+            diff = contains_char.difference(allowed_char)
             if len(diff) == 0:
                 res += 1
         return res
