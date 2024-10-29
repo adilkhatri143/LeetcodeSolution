72c312720b06f85578033dc8ef2a94c0cfa82956
adilkhatri143 <adilkhatri143@gmail.com>
2024-10-29 13:13:11 +0530

    Time: 445 ms (9.09%) | Memory: 25.8 MB (97.53%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.py
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.py
@@ -1,17 +1,17 @@
 class Solution:
     def maxMoves(self, grid: List[List[int]]) -> int:
-        ans = [[0 for i in range(len(grid[0]))] for j in range(len(grid))]
+        prev = [0 for j in range(len(grid))]
         max_ans = 0
         for col in range(len(grid[0]) - 2, -1, -1):
+            current = [0 for j in range(len(grid))]
             for row in range(len(grid)):
                 max_val = float("-inf")
                 if row > 0 and grid[row][col] < grid[row - 1][col + 1]:
-                    max_val = max(max_val, ans[row - 1][col + 1])
+                    max_val = max(max_val, prev[row - 1])
                 if grid[row][col] < grid[row][col + 1]:
-                    max_val = max(max_val, ans[row][col + 1])
+                    max_val = max(max_val, prev[row])
                 if row < len(grid) - 1 and grid[row][col] < grid[row + 1][col + 1]:
-                    max_val = max(max_val, ans[row + 1][col + 1])
-                ans[row][col] = 0 if max_val == float("-inf") else 1 + max_val
-                if col == 0:
-                    max_ans = max(max_ans, ans[row][col])
-        return max_ans
+                    max_val = max(max_val, prev[row + 1])
+                current[row] = 0 if max_val == float("-inf") else 1 + max_val
+            prev = current
+        return max(prev)
