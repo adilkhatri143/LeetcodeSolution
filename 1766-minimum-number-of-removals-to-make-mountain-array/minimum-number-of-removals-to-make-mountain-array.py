40840acbb0359103cda6dd1d2c1bc1816cbfbf69
adilkhatri143 <adilkhatri143@gmail.com>
2024-10-31 23:43:47 +0530

    Time: 34 ms (79.25%) | Memory: 16.9 MB (32.10%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
@@ -1,22 +1,33 @@
 class Solution:
+    def binary_search(self, lis, num):
+        low, high = 0, len(lis)
+        while low < high:
+            mid = (low + high) // 2
+            if lis[mid] < num:
+                low = mid + 1
+            else:
+                high = mid
+        return low
+    
+    def longest_increasing_subsequence(self, nums):
+        lis_len = [1] * len(nums)
+        lis = list()
+        for i in range(len(nums)):
+            idx = self.binary_search(lis, nums[i])
+            if idx == len(lis):
+                lis.append(nums[i])
+            else:
+                lis[idx] = nums[i]
+            lis_len[i] = len(lis)
+        return lis_len
+    
     def minimumMountainRemovals(self, nums: List[int]) -> int:
-        n = len(nums)
-        lis = [1] * n
-        for i in range(1, n):
-            max_inc = 0
-            for j in range(i - 1, -1, -1):
-                if nums[i] > nums[j]:
-                    max_inc = max(max_inc, lis[j])
-            lis[i] = 1 + max_inc
-        lds = [1] * n
-        for i in range(n - 2, -1, -1):
-            max_inc = 0
-            for j in range(i + 1, n):
-                if nums[i] > nums[j]:
-                    max_inc = max(max_inc, lds[j])
-            lds[i] = 1 + max_inc
-            min_remove = float("inf")
-        for i in range(n):
-            if lis[i] > 1 and lds[i] > 1:
-                min_remove = min(min_remove, n - (lis[i] + lds[i] - 1))
-        return min_remove
+        lis = self.longest_increasing_subsequence(nums)
+        nums.reverse()
+        lds = self.longest_increasing_subsequence(nums)
+        lds.reverse()
+        min_removal = float("inf")
+        for lis_val, lds_val in zip(lis, lds):
+            if lis_val > 1 and lds_val > 1:
+                min_removal = min(min_removal, len(nums) - (lis_val + lds_val - 1))
+        return min_removal
